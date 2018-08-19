a5c41be081c3accaa277c9d2807342e51bc6ff8d
Kevin Chen <kevin.zchn@gmail.com>
2018-08-19 17:20:43 -0400

    Add augur_flex cookiecutter template.

---
+++ augur_flex/augur_flex/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for augur-flex."""
+
+__author__ = """Kevin Chen"""
+__email__ = 'k_@berkeley.edu'
+__version__ = '0.1.0'
---
+++ augur_flex/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use augur-flex in a project::
+
+    import augur_flex
