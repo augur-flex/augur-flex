0e70bc5f63cd365f0084168b14df55d00099402e
Kevin Chen <kevin.zchn@gmail.com>
2018-09-02 13:58:29 -0400

    Add a public key environment variable.

--- scripts/10_export.sh
+++ scripts/10_export.sh
@@ -4,6 +4,7 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Private key for 0xe6ef0915f245bd9180a8388fe44fb3e93c331561
 export ETHEREUM_PRIVATE_KEY=0xd5a9eca3139cda1d5bcde9920fb797c3d7803ae80ee8f27881876fad29da9ca9
+export ETHEREUM_PUBLIC_KEY=0xe6ef0915f245bd9180a8388fe44fb3e93c331561
 export ETHEREUM_HTTP=http://127.0.0.1:8545
 
 # You don't normally need to modify this.
