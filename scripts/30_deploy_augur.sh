b3cb555841fdf7854585b523b946fe341fdaeefe
Kevin Chen <kevin.zchn@gmail.com>
2018-11-11 16:09:21 -0500

    Check whether subs/augur-core exists.

--- scripts/30_deploy_augur.sh
+++ scripts/30_deploy_augur.sh
@@ -2,10 +2,23 @@
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 . $SOURCE_DIR/10_export.sh
 
-wget -c https://github.com/ethereum/solidity/releases/download/v0.4.20/solc-static-linux -O bin/solc
+rm bin/solc
+wget -c https://github.com/ethereum/solidity/releases/download/v0.4.24/solc-static-linux -O bin/solc
 chmod +x bin/solc
 export PATH=`pwd`/bin:$PATH
+if [ -d "subs/augur-core" ]; then
+    echo "Building augur-core from subs/augur-core since it is present."
+    cd subs/augur-core
+    npm install
+    npm run build
+    cd ../../
+fi
 cd subs/augur.js
 npm install
+if [ -d "../augur-core" ]; then
+    echo "Using augur-core from subs/augur-core since it is present."
+    rm -fr node_modules/augur-core
+    ln -s `pwd`/../augur-core node_modules/augur-core
+fi
 echo "Deploying to " $ETHEREUM_HTTP
 npm run deploy:environment
